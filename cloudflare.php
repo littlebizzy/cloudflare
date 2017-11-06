2a18df025d319d3be06182581a7fa5c0e92bb259
Jesse Nickles <jesse@littlebizzy.com>
2017-11-05 17:52:46 -0800

    Update cloudflare.php

--- cloudflare.php
+++ cloudflare.php
@@ -1,29 +1,18 @@
 <?php
 /*
 Plugin Name: CloudFlare
-Plugin URI: http://www.cloudflare.com/wiki/CloudFlareWordPressPlugin
-Description: CloudFlare integrates your blog with the CloudFlare platform.
-Version: 1.3.24
-Author: Ian Pye, Jerome Chen, James Greene, Simon Moore, David Fritsch, John Wineman (CloudFlare Team)
-License: GPLv2
+Plugin URI: https://www.littlebizzy.com/plugins/cloudflare
+Description: Easily connect your WordPress website to CloudFlare's free optimization features, including one-click options to purge cache and enable 'dev' mode.
+Version: 1.0.0
+Author: LittleBizzy
+Author URI: https://www.littlebizzy.com
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+Prefix: CLDFLR
 */
 
 /*
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; version 2 of the License.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
 Plugin adapted from the Akismet WP plugin.
-
 */
 
 define('CLOUDFLARE_VERSION', '1.3.24');
