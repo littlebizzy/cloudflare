bd11aff8645ce09029f00dde61b76e54636e1e8b
Pau <pauiglesias@gmail.com>
2017-11-11 10:41:48 +0100

    Improved helper plugin class.

--- helpers/plugin.php
+++ helpers/plugin.php
@@ -3,13 +3,90 @@
 // Subpackage namespace
 namespace LittleBizzy\CloudFlare\Helpers;
 
+// Aliased plugin namespace
+use \LittleBizzy\CloudFlare;
+
 /**
  * Plugin class
  *
  * @package CloudFlare
  * @subpackage Helpers
  */
-class Plugin {
+final class Plugin {
+
+
+
+	// Properties
+	// ---------------------------------------------------------------------------------------------------
+
+
+
+	/**
+	 * Single class instance
+	 */
+	private static $instance;
+
+
+
+	/**
+	 * Basic plugin data
+	 */
+	public $path;
+	public $root;
+	public $prefix;
+	public $version;
+
+
+
+	// Initialization
+	// ---------------------------------------------------------------------------------------------------
+
+
+
+	/**
+	 * Create or retrieve instance
+	 */
+	public static function instance() {
+
+		// Check instance
+		if (!isset(self::$instance))
+			self::$instance = new self;
+
+		// Done
+		return self::$instance;
+	}
+
+
+
+	/**
+	 * Constructor
+	 */
+	private function __construct() {
+		$this->path 	= \LittleBizzy\CloudFlare\FILE;
+		$this->root 	= dirname($this->path);
+		$this->prefix 	= \LittleBizzy\CloudFlare\PREFIX;
+		$this->version 	= \LittleBizzy\CloudFlare\VERSION;
+	}
+
+
+
+	// Methods
+	// ---------------------------------------------------------------------------------------------------
+
+
+
+	/**
+	 * Handle the plugin hooks
+	 */
+	public function pluginHooks() {
+		$classname = __NAMESPACE__.'\\'.__CLASS__;
+		register_uninstall_hook($this->path, array($classname, 'uninstall'));
+	}
+
+
+
+	// WP Hooks
+	// ---------------------------------------------------------------------------------------------------
 
 
 
@@ -31,7 +108,7 @@ class Plugin {
 	 * Plugin uninstall
 	 */
 	public static function uninstall() {
-		\LittleBizzy\CloudFlare\Core\Data::remove();
+		Core\Data::remove();
 	}
 
 
