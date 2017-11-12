b41458be55d63980c40751604631691d2f6ef144
Pau <pauiglesias@gmail.com>
2017-11-12 14:37:57 +0100

    Removed all status and replaced by zone data.

--- core/data.php
+++ core/data.php
@@ -42,33 +42,7 @@ final class Data {
 	public $key;
 	public $email;
 	public $domain;
-	public $status;
-	public $devmode;
-	public $devmodeStatus;
-
-
-
-	/**
-	 * Statuses for domain
-	 */
-	private static $statuses = array(
-		'unknown' 	=> 'Unknown status',
-		'error'	  	=> 'Unable to retrieve your domain from CloudFlare API',
-		'mismatch' 	=> 'Your domain does not match the CloufFlare API domain',
-		'valid'	  	=> 'Your domain matches the CloudFlare API settings',
-	);
-
-
-
-	/**
-	 * Statuses for the DEV MODE
-	 */
-	private static $devmodeStatuses = array(
-		'unknown'  => 'Unknown status',
-		'error'    => 'Unable to retrieve the DEV Mode status',
-		'disabled' => 'DEV Mode disabled',
-		'enabled'  => 'DEV Mode enabled',
-	);
+	public $zone;
 
 
 
@@ -125,23 +99,9 @@ final class Data {
 	 * Load data
 	 */
 	public function load()  {
-
-		// Domain
-		$this->key 		= $this->options->get('key', true);
-		$this->email 	= $this->options->get('email', true);
-		$this->status 	= $this->options->get('status', true);
-
-		// Validate status
-		if (!empty($this->status) && !isset(self::$statuses[$this->status]))
-			$this->status = 'unknown';
-
-		// DEV Mode
-		$this->devmode 			= $this->options->get('devmode', true);
-		$this->devmodeStatus 	= $this->options->get('devmode_status', true);
-
-		// Validate DEV Mode status
-		if (!empty($this->devmodeStatus) && !isset(self::$devmodeStatuses[$this->devmodeStatus]))
-			$this->devmodeStatus = 'unknown';
+		$this->key 	 = $this->options->get('key', true);
+		$this->email = $this->options->get('email', true);
+		$this->zone  = @json_decode($this->options->get('zone', true), true);
 	}
 
 
@@ -163,23 +123,9 @@ final class Data {
 		if (isset($values['email']))
 			$this->options->set('email', $values['email'], false, true);
 
-		// Check and validate domain status
-		if (isset($values['status'])) {
-			if (!isset(self::$statuses[$values['status']]))
-				$values['status'] = 'unknown';
-			$this->options->set('status', $values['status'], false, true);
-		}
-
-		// Check DEV Mode value
-		if (isset($values['devmode']))
-			$this->options->set('devmode', $values['devmode'], false, true);
-
-		// Check and validate DEV Mode status
-		if (isset($values['devmode_status'])) {
-			if (!isset(self::$devmodeStatuses[$values['devmode_status']]))
-				$values['devmode_status'] = 'unknown';
-			$this->options->set('devmode_status', $values['devmode_status'], false, true);
-		}
+		// Check zone value
+		if (isset($values['zone']))
+			$this->options->set('zone', @json_encode($values['zone']), false, true);
 
 		// Check reload
 		if ($reload)
@@ -192,7 +138,7 @@ final class Data {
 	 * Remove options from database
 	 */
 	public function remove() {
-		$this->options->del(['key', 'email', 'status', 'devmode', 'devmode_status']);
+		$this->options->del(['key', 'email', 'zone']);
 	}
 
 
