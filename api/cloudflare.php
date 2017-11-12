b942088cc10ec9c866b8f44ce4b48548557e1089
Pau <pauiglesias@gmail.com>
2017-11-12 19:06:50 +0100

    Changed the way some methods are called.

--- api/cloudflare.php
+++ api/cloudflare.php
@@ -91,12 +91,9 @@ final class Cloudflare {
 	 * Retrieve current zones
 	 */
 	public function getZones($page = 1, $per_page = 50) {
-		return $this->request('zones', [
-			'method' 	=> 'GET',
-			'body' 		=> [
-				'page' 		=> $page,
-				'per_page' 	=> $per_page,
-			],
+		$endpoint = add_query_arg(['page' => $page, 'per_page' => $per_page], 'zones');
+		return $this->request($endpoint, [
+			'method' => 'GET',
 		]);
 	}
 
@@ -107,7 +104,19 @@ final class Cloudflare {
 	 */
 	public function getZone($zoneId) {
 		return $this->request('zones/'.$zoneId, [
-			'method' 	=> 'GET',
+			'method' => 'GET',
+		]);
+	}
+
+
+
+	/**
+	 * Set dev mode
+	 */
+	public function setDevMode($zoneId, $enable) {
+		return $this->request('zones/'.$zoneId.'/settings/development_mode', [
+			'method' => 'PATCH',
+			'body' => @json_encode(['value' => $enable? 'on' : 'off'])
 		]);
 	}
 
@@ -158,203 +167,11 @@ final class Cloudflare {
 			return $this->newError('cloudflare_api_error_response', $result);
 
 		// Check result
-		if (empty($response['result']) || empty($response['result_info']) || 'true' != $response['success'])
+		if (empty($response['result']) || 'true' != $response['success'])
 			return $this->newError('cloudflare_api_error_result', $response);
 
 		// Done
 		return $response;
-	}
-
-
-
-
-
-	/**
-	 * Retrieve associated domain
-	 */
-	public function getDomain() {
-
-		// Perform request
-		$result = $this->request([
-			'a' => 'zone_load_multi',
-		]);
-
-		// Direct error
-		if (is_wp_error($result)) {
-			$this->error = $result;
-			return false;
-		}
-//print_r($result);
-		// Check zones
-		$zones = (empty($result->response->zones->objs) || !is_array($result->response->zones->objs))? array() : $result->response->zones->objs;
-		if (0 == count($zones)) {
-			$this->error = $this->newError('match_domain', 'API did not return any domains');
-			return false;
-		}
-
-		// Collect
-		$names = [];
-		foreach ($zones as $zone) {
-			if (!empty($zone->zone_name))
-				$names[strtolower($zone->zone_name)] = $zone;
-		}
-
-		// Check values
-		if (empty($names)) {
-			$this->error = $this->newError('match_domain', 'API did not return any domains');
-			return false;
-		}
-
-		// Check if exists
-		$domain = strtolower($this->domain);
-		if (isset($names[$domain]))
-			return $names[$domain];
-
-		// Now see match_domain_to_zone
-		$this->error = $this->newError('match_domain', 'API did not match any domains');
-		return false;
-	}
-
-
-/**
- * @param $domain        string      the domain portion of the WP URL
- * @param $zone_names    array       an array of zone_names to compare against
- *
- * @returns null|string null in the case of a failure, string in the case of a match
- */
-function match_domain_to_zone($domain, $zones) {
-
-    $splitDomain = explode('.', $domain);
-    $totalParts = count($splitDomain);
-
-    // minimum parts for a complete zone match will be 2, e.g. blah.com
-    for ($i = 0; $i <= ($totalParts - 2); $i++) {
-        $copy = $splitDomain;
-        $currentDomain = implode('.', array_splice($copy, $i));
-        foreach ($zones as $zone_name) {
-            if (strtolower($currentDomain) == strtolower($zone_name)) {
-                return $zone_name;
-            }
-        }
-    }
-
-    return null;
-}
-
-
-	/**
-	 * Retrieve DEV MODE status
-	 */
-	public function getDevMode() {
-
-		// Perform request
-		$result = $this->request([
-			'a' => 'zone_load',
-			'z' => $this->host,
-		]);
-
-		// Check error
-		if (is_wp_error($result)) {
-			$this->error = $result;
-			return false;
-		}
-
-		// Need improvements
-		return isset($result->response->zone->obj->zone_status_class)? $result->response->zone->obj->zone_status_class : false;
-	}
-
-
-
-	/**
-	 * Check DEV MODE status value
-	 */
-	public function isDevMode($value) {
-		// See if value match "status-dev-mode"
-	}
-
-
-
-	/**
-	 * Update DEV MODE status
-	 *
-	 * @param $value	boolean
-	 */
-	function setDevMode($value) {
-
-		// Perform request
-		$result = $this->request([
-			'a' => 'devmode',
-	        'z' => $this->host,
-	        'v' => $value? 'status-dev-mode' : 'other-value?',
-		]);
-
-		// Check error
-	    if (is_wp_error($result)) {
-			$this->error = $result;
-			return false;
-		}
-
-		// Done
-	    return $result;
-	}
-
-
-
-
-
-
-	/**
-	 * API Request
-	 */
-	private function request_old($fields, $json = true) {
-
-		// Check credentials
-		if (empty($this->key) || empty($this->email))
-			$this->newError('missing_credentials', 'API credentials must be established before the API request call.');
-
-		// Prepare fields
-		$fields = array_merge($fields, [
-			'tkn'	=> $this->key,
-			'email'	=> $this->email,
-		]);
-
-		// Perform request
-	    $response = wp_remote_post(self::ENDPOINT_URL, [
-			'body'			=> $fields,
-	        'timeout'       => 20,
-	        'sslverify'     => true,
-	        'user-agent'    => 'CloudFlare/WordPress/1.3.24',
-	    ]);
-
-		// Check error
-		if (is_wp_error($response))
-			return $response;
-
-		// Check results
-		if (!is_array($response))
-			$this->newError('unknown_wp_http_error', sprintf('Unknown response from wp_remote_post - unable to contact CloudFlare API'));
-
-		// Check HTTP code
-		if (200 != (int) $response['response']['code'])
-			return $this->newError('cloudflare', sprintf('CloudFlare API returned a HTTP Error: %s - %s', $response['response']['code'], $response['response']['message']));
-
-		// Check output
-		if (!$json)
-			return $response;
-
-		// Decode from JSON
-		$result = @json_decode($response['body']);
-		if (empty($result))
-			return $this->newError('json_decode', sprintf('Unable to decode JSON response'), $response['body']);
-
-		// Check for the CloudFlare API failure response
-		if (property_exists($result, 'result') && $result->result != 'success') {
-			$msg = (property_exists($result, 'msg') && !empty($result->msg))? $result->msg : 'Unknown Error';
-			return $this->newError('cloudflare', $msg);
-		}
-
-		// Done
-		return $result;
 	}
 
 
