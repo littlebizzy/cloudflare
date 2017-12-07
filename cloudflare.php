d56f8ffe757eb638d7912d5258b12439d200d06a
Jesse Nickles <jesse@littlebizzy.com>
2017-12-06 17:13:51 -0800

    Update cloudflare.php

--- cloudflare.php
+++ cloudflare.php
@@ -3,7 +3,7 @@
 Plugin Name: CloudFlare
 Plugin URI: https://www.littlebizzy.com/plugins/cloudflare
 Description: Easily connect your WordPress website to CloudFlare's free optimization features, including one-click options to purge cache and enable 'dev' mode.
-Version: 1.0.0
+Version: 1.0.1
 Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
 License: GPLv3
@@ -14,14 +14,18 @@ Prefix: CLDFLR
 // Plugin namespace
 namespace LittleBizzy\CloudFlare;
 
-// Avoid script calls via plugin URL
+// Admin Notices module
+require_once dirname(__FILE__).'/admin-notices.php';
+CLDFLR_Admin_Notices::instance(__FILE__);
+
+// Block direct calls
 if (!function_exists('add_action'))
 	die;
 
 // Plugin constants
 const FILE = __FILE__;
 const PREFIX = 'cldflr';
-const VERSION = '1.0.0';
+const VERSION = '1.0.1';
 
 // Loader
 require_once dirname(FILE).'/helpers/autoload.php';
